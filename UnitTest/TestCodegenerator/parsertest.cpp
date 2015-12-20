a89d1f3c2138bea6a4f2a27c9bf6e0dce963acd5
Udo Herrmann <herrmann.udo@gmx.de>
2015-12-20 20:24:56 +0100

    first unittest parser

--- UnitTest/TestCodegenerator/parsertest.cpp
+++ UnitTest/TestCodegenerator/parsertest.cpp
@@ -2,10 +2,16 @@
 #include "../../gtest/gtest.h"
 //#include "../../gmock/gmock.h"
 #include "Parserimplementation.h"
+#include <vector>
+#include <string>
 
 ParserTest::ParserTest()
 {
-    ParserImpl *parser = new ParserImpl();
-    delete parser;
 }
 
+TEST(ParserTest, ReadFile) {
+    ParserImpl *parser = new ParserImpl();
+    std::vector<std::string> value = parser->doParse("..\\Files\\TestParser.txt");
+    EXPECT_EQ("Parser Test successfully done", value[0]);
+    delete parser;
+}
