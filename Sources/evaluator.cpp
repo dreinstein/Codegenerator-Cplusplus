54fcad2ec44c9f7cc560792562318c3ad65c3d5f
herrmann.udo@gmx.de <herrmann.udo@gmx.de>
2015-12-24 12:48:56 +0100

    add function and testfunctionality for evaluation of script against keywords

--- Sources/evaluator.cpp
+++ Sources/evaluator.cpp
@@ -1,4 +1,10 @@
 #include "Evaluator.h"
+#include <vector>
+
+using namespace std;
+
+namespace NGenerator
+{
 
 ScriptEvaluator::ScriptEvaluator()
 {
@@ -12,13 +18,55 @@ ScriptEvaluator::~ScriptEvaluator()
 
 bool ScriptEvaluator::evaluate(std::vector<std::string> vkeywords, std::vector<std::string> vscript)
 {
-    // iteriere über script
-    // finde Wort mit Beginn @ und merke
-    // iteriere über keywords
-    // wenn übereinstimmung keyword mit wort aus Script
-    // dann: beende iteration über Keyword. 1 weiter mit script
-    // wenn Wort nicht gefunden Abbruch mit Rückgabe false
-    return false;
+    string scriptentry = "noVal";
+    bool found = false;
+    for (auto itscript = vscript.begin(); itscript != vscript.end(); ++itscript)
+    {
+        scriptentry = extractString(*itscript);
+        found = findScriptentryInKeyword(scriptentry,vkeywords);
+        if (found == false)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool ScriptEvaluator::findScriptentryInKeyword(std::string entry,std::vector<std::string> vkeywords)
+{
+
+    bool found = false;
+    for (auto itkeywords = vkeywords.begin(); itkeywords != vkeywords.end(); ++itkeywords)
+    {
+        if(*itkeywords == entry)
+        {
+            found = true;
+            break;
+        }
+    }
+    if (found == false)
+    {
+        return false;
+    }
+    return true;
+}
+
+
+std::string ScriptEvaluator::extractString(std::string ostring)
+{
+    // get rid of @
+    ostring.erase (0,1);
+    std::size_t foundSeperator =  UINT_MAX;
+    // find seperator
+    foundSeperator = ostring.find(SEPERATOR);
+    // earse after seperator
+    if (foundSeperator  < UINT_MAX)
+    {
+        ostring.erase(foundSeperator,ostring.length());
+    }
+    return ostring;
+}
+
 }
 
 
--- Sources/evaluator.h
+++ Sources/evaluator.h
@@ -2,17 +2,25 @@
 #define EVALUATOR_H
 
 #include "BaseEvaluator.h"
+#include <string>
 
-
+namespace NGenerator
+{
 
 class ScriptEvaluator:public BaseEvaluator
 {
+private:
+    std::string extractString(std::string ostring);
+    bool findScriptentryInKeyword(std::string entry,std::vector<std::string> vkeywords);
+    const std::string SEPERATOR = "::";
 public:
     ScriptEvaluator();
     virtual ~ScriptEvaluator();
     bool evaluate(std::vector<std::string> vkeywords, std::vector<std::string> vscript);
     bool evaluate();
 };
+
+}
 
 
 #endif // EVALUATOR_H
