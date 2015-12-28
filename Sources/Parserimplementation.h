655442d0e933b6e6c4e3fbb65ec2b391f09b97e8
Udo Herrmann <herrmann.udo@gmx.de>
2015-12-28 15:24:06 +0100

    clean

--- Sources/Parserimplementation.h
+++ Sources/Parserimplementation.h
@@ -3,12 +3,9 @@
 
 #include <vector>
 #include <string>
-
-#include "parserimplementationbase.h"
-
-
-namespace NParser
-{
+#include "BaseParserImplementation.h"
+//namespace NParser
+//{
 
 class ParserImpl:public BaseParserImpl
 {
@@ -18,5 +15,5 @@ public:
     std::vector<std::string> doParse(std::string);
 };
 
-}
+//}
 #endif // PARSERIMPLEMENTATION_H
